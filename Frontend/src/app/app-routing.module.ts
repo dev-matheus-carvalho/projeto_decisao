82ea86f55d32d9a0345434a5ffebd69443a0fb18
Matheus Carvalho <matheuscarvalho@gmail.com>
2023-11-21 18:54:20 -0300

    Refatoração dos componentes shared

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BemvindoComponent } from './components/bemvindo/bemvindo.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
-  {path: 'teste', component: BemvindoComponent}
+  {path: 'teste', component: BemvindoComponent},
+  {path: 'register', component: RegisterComponent},
 ];
 
 @NgModule({
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,7 @@ import { HeaderComponent } from './shared/header/header.component';
 import { MenuComponent } from './shared/menu/menu.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { BemvindoComponent } from './components/bemvindo/bemvindo.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +16,13 @@ import { BemvindoComponent } from './components/bemvindo/bemvindo.component';
     HeaderComponent,
     MenuComponent,
     FooterComponent,
-    BemvindoComponent
+    BemvindoComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
