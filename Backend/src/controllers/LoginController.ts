923aef57db7170ea166b50435ad4d8d281246441
Matheus Carvalho <matheuscarvalho@gmail.com>
2023-11-22 16:18:02 -0300

    Refatorando mensagem de LoginController Backend

--- Backend/src/controllers/LoginController.ts
+++ Backend/src/controllers/LoginController.ts
@@ -9,12 +9,12 @@ export async function Login(request: Request, response: Response) {
     const usuario = await loginUsuario(email, senha);
 
     if (usuario === true) {
-      response.status(404).json('Acesso negado. Verifique seu email');
+      response.status(404).json('E-mail inserido incorreto. Tente novamente');
       return;
     }
 
     if (usuario === false) {
-      response.status(404).json('Acesso negado. Verifique sua senha');
+      response.status(404).json('Senha inserida incorreta. Tente novamente');
       return;
     }
     return response.status(200).json(usuario);
