cbcf6fd4b773cd242096cdc83d3262b04d30ebb2
Matheus Carvalho <matheuscarvalho@gmail.com>
2023-11-24 11:22:11 -0300

    cabeçalho principal de clientes

--- Frontend/src/app/components/clientes/clientes-routing.module.ts
+++ Frontend/src/app/components/clientes/clientes-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientesComponent } from './clientes.component';
+import { ClientesReadComponent } from './clientes-read/clientes-read.component';
 
 const clienteRoutes: Routes = [
-  { path: '', component: ClientesComponent },
+  { path: '', component: ClientesComponent, children: [
+    { path: 'listar', component: ClientesReadComponent }
+  ] },
 ];
 
 @NgModule({
