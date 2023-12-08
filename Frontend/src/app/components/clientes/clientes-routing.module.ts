69285acaef8b68db6dbbb3c8d7e09ce242f32907
Matheus Carvalho <matheuscarvalho@gmail.com>
2023-12-08 17:52:05 -0300

    Ultimo commmit: Entrega 08/12/2023

--- Frontend/src/app/components/clientes/clientes-routing.module.ts
+++ Frontend/src/app/components/clientes/clientes-routing.module.ts
@@ -4,19 +4,26 @@ import { ClientesComponent } from './clientes.component';
 import { ClientesReadComponent } from './clientes-read/clientes-read.component';
 import { ClientesCreateComponent } from './clientes-create/clientes-create.component';
 import { FormularioComponent } from './clientes-create/formulario/formulario.component';
-import { LocalizacaoComponent } from './clientes-create/localizacao/localizacao.component';
+import { LocalizacaoComponent } from './clientes-update/localizacao/localizacao.component';
 import { RepresentantesComponent } from './clientes-create/representantes/representantes.component';
+import { ClientesUpdateComponent } from './clientes-update/clientes-update.component';
 
 const clienteRoutes: Routes = [
   { path: '', component: ClientesComponent, children: [
     { path: 'listar', component: ClientesReadComponent },
+
   ] },
+
   { path: 'criar', component: ClientesCreateComponent, children: [
     { path: 'formulario', component: FormularioComponent },
     { path: 'localizacao', component: LocalizacaoComponent },
-    { path: 'representantes', component: RepresentantesComponent },
+  ] },
+
+  { path: 'update', component: ClientesUpdateComponent, children: [
+    { path: 'localizacao', component: LocalizacaoComponent }
   ] },
 ];
+
 
 @NgModule({
   imports: [RouterModule.forChild(clienteRoutes)],
--- Frontend/src/app/components/clientes/clientes.module.ts
+++ Frontend/src/app/components/clientes/clientes.module.ts
@@ -7,9 +7,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { ClientesReadComponent } from './clientes-read/clientes-read.component';
 import { ClientesCreateComponent } from './clientes-create/clientes-create.component';
-import { LocalizacaoComponent } from './clientes-create/localizacao/localizacao.component';
+import { LocalizacaoComponent } from './clientes-update/localizacao/localizacao.component';
 import { RepresentantesComponent } from './clientes-create/representantes/representantes.component';
 import { FormularioComponent } from './clientes-create/formulario/formulario.component';
+import { ClientesUpdateComponent } from './clientes-update/clientes-update.component';
 
 
 @NgModule({
@@ -17,6 +18,7 @@ import { FormularioComponent } from './clientes-create/formulario/formulario.com
     ClientesComponent,
     ClientesReadComponent,
     ClientesCreateComponent,
+    ClientesUpdateComponent,
     LocalizacaoComponent,
     RepresentantesComponent,
     FormularioComponent
@@ -25,7 +27,7 @@ import { FormularioComponent } from './clientes-create/formulario/formulario.com
     CommonModule,
     FormsModule,
     HttpClientModule,
-    ClientesRoutingModule
+    ClientesRoutingModule,
   ]
 })
 export class ClientesModule { }
