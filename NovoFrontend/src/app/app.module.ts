7bcf88585e69f6214c0e44bb5c73a387cfaef971
Matheus Carvalho <matheuscarvalho@gmail.com>
2023-12-15 13:53:43 -0300

    Criação parte 1 do desenvolvimento: Menu, cabecalho e footer

--- NovoFrontend/src/app/app.module.ts
+++ NovoFrontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './shared/components/cabecalho/cabecalho.component';
+import { MenuComponent } from './shared/components/menu/menu.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
