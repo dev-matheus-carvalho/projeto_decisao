8fbd5b0b9aa4d2a80b2046713f33f16e24b67fa5
Matheus Carvalho <matheuscarvalho@gmail.com>
2023-12-18 17:46:49 -0300

    Refatorando o footer e o menu

--- NovoFrontend/src/app/shared/components/menu/menu.component.ts
+++ NovoFrontend/src/app/shared/components/menu/menu.component.ts
@@ -1,22 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
 
   public isHomeOn: boolean = true;
   public isClientOn: boolean = false
 
+  ngOnInit(): void {
+    const page = localStorage.getItem('page');
+    console.log(page);
+
+    if (page === 'home') {
+      this.home()
+    } else {
+      this.client();
+    };
+  }
+
   public home(): void {
     this.isHomeOn = true;
     this.isClientOn = false;
+    localStorage.setItem('page', 'home');
   }
 
   public client(): void {
     this.isHomeOn = false;
     this.isClientOn = true;
+    localStorage.setItem('page', 'clients');
   }
 }
